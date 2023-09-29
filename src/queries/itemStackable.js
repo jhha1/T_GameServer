768f12b786dbce680bd00cb776ca47188e6fa0d0
jhha1 <skgkwlgp@gmail.com>
2023-09-29 20:37:37 +0900

    친선전 - 승리 쿼리 변경

--- src/queries/itemStackable.js
+++ src/queries/itemStackable.js
@@ -3,6 +3,7 @@ module.exports = {
     selectWithId: `SELECT user_id, item_id, count FROM ItemStackable WHERE user_id = ? AND item_id = ?;`,
     insert: `INSERT INTO ItemStackable (user_id, item_id, count) VALUES (?, ? , ?);`,
     update: `UPDATE ItemStackable SET count = ? WHERE user_id = ? AND item_id = ?;`,
+    increase: `UPDATE ItemStackable SET count = count + ? WHERE user_id = ? AND item_id = ?;`,
     insertMany: (rowCount) => {
         let q = `INSERT INTO ItemStackable (user_id, item_id, count) VALUES `;
         for (let i = 0; i < rowCount; i++) {
--- src/queries/stage.js
+++ src/queries/stage.js
@@ -2,6 +2,6 @@ module.exports = {
     selectByUserId: `SELECT user_id, season, score, win, lose FROM Stage WHERE user_id = ?;`,
     selectByUserIdAndSeason: `SELECT user_id, season, score, win, lose FROM Stage WHERE user_id = ? AND season = ?;`,
     insert: `INSERT INTO Stage (user_id, season, score, win, lose ) VALUES (?, ? ,?, ? ,?);`,
-    updateWin: `UPDATE Stage SET win = win + 1, score = ? WHERE user_id = ? AND season = ?;`,
+    updateWin: `UPDATE Stage SET win = win + 1, score = score + ? WHERE user_id = ? AND season = ?;`,
     updateLose: `UPDATE Stage SET lose = lose + 1 WHERE user_id = ? AND season = ?;`,
 }
