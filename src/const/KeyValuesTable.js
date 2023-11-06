84996e6109e49864dae5a550eb0f0b2dc459849c
jhha1 <skgkwlgp@gmail.com>
2023-11-07 02:20:30 +0900

    const table - key values bug fix

--- src/const/KeyValuesTable.js
+++ src/const/KeyValuesTable.js
@@ -6,10 +6,10 @@ const keyValuesTable = {
         const rows = helper.getTable("KeyValues");
         for (let row of rows) {
             if (Number(row.type) === 1) { // primitive int values
-                keyValuesTable._map[row.key] = Number(row.value);
+                keyValuesTable._map[row.key] = Number(row.values);
             }
             if (Number(row.type) === 2) { // array
-                keyValuesTable._map[row.key] = Array.isArray(row.value)? row.value : JSON.parse(row.value);
+                keyValuesTable._map[row.key] = Array.isArray(row.values)? row.value : JSON.parse(row.values);
             }
         }
     },
